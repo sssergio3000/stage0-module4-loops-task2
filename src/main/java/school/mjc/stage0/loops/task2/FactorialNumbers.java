4442f9f335312953142eb9c88e8c914eec4e5f26
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-27 13:23:41 +0200

    update2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,8 +3,14 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
         int fact = 1;
-        int count = 1;
+        int count = 0;
         while (count <= printToInclusive) {
+            if (count==0){
+                System.out.println(1);
+                count++;
+                continue;
+            }
+
             fact *= count++;
             System.out.println(fact);
 
@@ -16,6 +22,6 @@ public class FactorialNumbers {
 
     public static void main(String[] args) {
         FactorialNumbers fn = new FactorialNumbers();
-        fn.printFactorialRow(5);
+        fn.printFactorialRow(0);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,32 +3,29 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int count = 0;
-        int mult = 1;
-        if (multiplyByAndToInclusive > 0) {
-            while (count <= multiplyByAndToInclusive) {
-                mult *= count++;
-
-                System.out.println(mult);
-
-                if (mult==0){
-                    mult=1;
-                }
+        if(multiplyByAndToInclusive>0){
+            while (count <= multiplyByAndToInclusive){
+                System.out.println(count*multiplyByAndToInclusive);
+                count++;
             }
 
+
         }
-        if (multiplyByAndToInclusive < 0) {
-            while (count >= multiplyByAndToInclusive) {
-                mult *= count;
+        else {
+            while (count >= multiplyByAndToInclusive){
+                System.out.println(count*-multiplyByAndToInclusive);
                 count--;
-                System.out.println(mult);
             }
 
         }
 
-    }
+
+        }
+
+
 
     public static void main(String[] args) {
         Multiplication m = new Multiplication();
-        m.printMultiplied(5);
+        m.printMultiplied(0);
     }
 }
